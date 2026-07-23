9b4543314e45728921720569a653506d00e1d95b
nikhil-agarwal9829 <nikhilagarwal9785@gmail.com>
2026-07-23 22:41:41 +0530

    Time: 20 ms (8.89%), Space: 46.9 MB (5.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length())
-            return false;
+        
         HashMap<Character,Integer> map=new HashMap<>();
         for(int i=0;i<s.length();i++){
             map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
