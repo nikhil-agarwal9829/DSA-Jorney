7482a24817c8a0456e69f4110b236ee7b6f0e2be
nikhil-agarwal9829 <nikhilagarwal9785@gmail.com>
2026-07-31 00:44:09 +0530

    Time: 0 ms (100%), Space: 42.5 MB (78.29%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    int[] dp;
+
+    public int combinationSum4(int[] nums, int target) {
+
+        dp = new int[target + 1];
+
+        Arrays.fill(dp, -1);
+
+        return solve(nums, target);
+    }
+
+    int solve(int[] nums, int target){
+
+        if(target == 0)
+            return 1;
+
+        if(target < 0)
+            return 0;
+
+        if(dp[target] != -1)
+            return dp[target];
+
+        int ways = 0;
+
+        for(int num : nums){
+            ways += solve(nums, target - num);
+        }
+
+        dp[target] = ways;
+
+        return dp[target];
+    }
+}
