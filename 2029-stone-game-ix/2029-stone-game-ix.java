8a3d6899a922a3da16c32cd30c9336c92e720798
nikhil-agarwal9829 <nikhilagarwal9785@gmail.com>
2026-08-16 20:29:52 +0530

    Time: 3 ms (100%), Space: 114.5 MB (94.59%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public boolean stoneGameIX(int[] stones) {
+
+        int[] count = new int[3];
+
+        for (int x : stones) {
+            count[x % 3]++;
+        }
+
+        if (count[1] == 0 && count[2] == 0) {
+            return false;
+        }
+
+
+        if (count[0] % 2 == 0) {
+            return count[1] > 0 && count[2] > 0;
+        }
+
+        return Math.abs(count[1] - count[2]) > 2;
+    }
+}
