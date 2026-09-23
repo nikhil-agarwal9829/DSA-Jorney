bdf121b57e3855f08d0656695ff9c62a9edf8867
Nikhil Agarwal <nikhilagarwal9785@gmail.com>
2026-09-23 23:54:03 +0530

    Time: 356 ms (80.02%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,7 @@
+SELECT id,
+       CASE
+           WHEN id % 2 = 1
+               THEN COALESCE(LEAD(student) OVER (ORDER BY id), student)
+           ELSE LAG(student) OVER (ORDER BY id)
+       END AS student
+FROM Seat;
